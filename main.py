fdad79863ed62f697cbab613e8fbb731e058903b
Ziyuan Wei <ziyuan3@illinois.edu>
2021-07-25 17:54:38 -0500

    Push relevant files to repo

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from typing import List
+import time
+import asyncio
+import ocr
+import utils
 
 app = FastAPI()
 
 @app.get("/")
-def root():
-  return {"message": "Hello World"}
+def home():
+    return {"message": "Visit the endpoint: /api/v1/extract_text to perform OCR."}
+
+@ app.post("/")
+def analyze_text(text: Model):
+    response = {"sentiment": [], "keyphrases": []}
+    no_of_text = len(text.text_to_analyze)
+    for i in range(no_of_text):
+        document = {"documents": [{"id": i+1, "language": "en", "text": text.text_to_analyze[i]}]}
+        
+        sentiment = utils.call_text_analytics_api(headers, document, endpoint='sentiment')
+        keyphrases = utils.call_text_analytics_api(headers, document, endpoint='keyPhrases')
+        
+        response["sentiment"].append(sentiment["documents"][0])
+        response["keyphrases"].append(keyphrases["documents"][0])
+    return response
